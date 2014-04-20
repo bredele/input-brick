40c5e1e6e2961df26af7b6d96da02a4dcd74484d
bredele <olivier.wietrich@gmail.com>
2014-04-19 22:26:22 -0600

    standalone is working but needs some refactoring though

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+
+/**
+ * Module dependencies.
+ * @api private
+ */
 var events = require('sewer');
 var Store = require('datastore');
 
+
 /**
- * Expose 'input-brick'
+ * Expose 'wired'
  */
 
 module.exports = wired;
 
 
+/**
+ * Double bind node element (input type).
+ *
+ * If data's attribute is not specified, 
+ * wired will automatically look for a name
+ * attribute.
+ * 
+ * Examples:
+ *
+ *   wired(el);
+ *   wired(el, 'label');
+ *   wired(el, 'label', data);
+ *   
+ * @param  {Element} node 
+ * @param  {String} name 
+ * @param  {Object | Datastore} data 
+ * @return {Datastore}
+ * @api public
+ */
 
 function wired(node, name, data) {
+
 	var store = new Store(data);
 	if(typeof name === 'object') {
 		data = name;
@@ -23,14 +49,10 @@ function wired(node, name, data) {
 	events.attach(node, 'change', change);
 	events.attach(node, 'keydown', change);
 	events.attach(node, 'keyup', change);
+
+	var value = store.get(name);
+	if(value) node.value = value;
+
 	return store;
 }
-
-
-// function model(node, expr) {
-// 	var view = this;
-// 	ev(node, 'input', function(node) {
-// 		view.set(expr, node.value);
-// 	});
-// };
 
